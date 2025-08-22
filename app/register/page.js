b0a35c3fa4522e51fc44392ca0ab83b3dae651f8
BothSann <boththann76@gmail.com>
2025-08-22 15:06:24 +0700

    Remove LoginForm and RegisterForm components along with their associated pages to streamline the application structure.

--- app/register/page.js
+++
@@ -1,9 +0,0 @@
-import RegisterForm from "@/components/RegisterForm";
-
-export default function RegisterPage() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <RegisterForm />
-    </div>
-  );
-}
